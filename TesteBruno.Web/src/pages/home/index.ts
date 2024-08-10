25dc9805ed6745109e61cce91d661d3abfeeb2e8
thofalbo <thomazfalbo@outlook.com>
2024-08-10 13:58:00 -0300

    implementando node e uikit

---
+++ TesteBruno.Web/src/pages/home/index.ts
@@ -0,0 +1,7 @@
+import './index.less';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
+
+console.log("typescript configurado");
---
+++ TesteBruno.Web/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    watch: false,
+    devtool: 'source-map',
+    entry: {
+        home: './src/pages/home/index.ts'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ]
+            },
+            {
+                test: /\.ico$/,
+                use: 'file-loader?name=[name].[ext]'
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.css'],
+        alias: {
+            'jquery-ui-css': 'jquery-ui-dist/jquery-ui.min.css',
+            'jquery-ui': 'jquery-ui-dist/jquery-ui',
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            'components': path.resolve(__dirname, 'src/components'),
+            'helpers': path.resolve(__dirname, 'src/helpers'),
+            'styles': path.resolve(__dirname, 'src/styles'),
+            'uikit-css': 'uikit/dist/css/uikit.min.css'
+        }
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, 'wwwroot', 'dist'),
+        library: ['testebruno', '[name]'],
+    }
+};
